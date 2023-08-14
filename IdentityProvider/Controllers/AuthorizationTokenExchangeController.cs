93f6acc414bed6d7bfabb37cad44e5ad009ab87c
damienbod <damien_bod@hotmail.com>
2023-08-14 13:52:31 +0200

    code clean up

--- IdentityProvider/Controllers/AuthorizationTokenExchangeController.cs
+++ IdentityProvider/Controllers/AuthorizationTokenExchangeController.cs
@@ -88,7 +88,7 @@ public class AuthorizationTokenExchangeController : Controller
         }
 
         // use data and return new access token
-        var (ActiveCertificate, _) = await Startup.GetCertificates(_environment, _configuration);
+        var (ActiveCertificate, _) = await HostingExtensions.GetCertificates(_environment, _configuration);
 
         var tokenData = new CreateDelegatedAccessTokenPayloadModel
         {
