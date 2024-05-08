03b3dde9832cd7fdf5e38cb2e178e9a5180e0eb1
damienbod <damien_bod@hotmail.com>
2024-05-08 10:08:21 +0200

    Update claims handling

--- OAuthGrantExchangeIntegration/Server/ValidateOauthTokenExchangeRequestPayload.cs
+++ OAuthGrantExchangeIntegration/Server/ValidateOauthTokenExchangeRequestPayload.cs
@@ -59,7 +59,7 @@ public static class ValidateOauthTokenExchangeRequestPayload
 
             var tokenValidator = new JsonWebTokenHandler
             {
-                MapInboundClaims = true
+                MapInboundClaims = false
             };
 
             var tokenValidationResult = await tokenValidator.ValidateTokenAsync(jwtToken, validationParameters);
